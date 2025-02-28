b2c2e518470973469d05022febb7d4c6ce48cd03
enisabazi <enis.abazi@starlabspro.com>
2025-02-28 23:00:17 +0100

    wip/

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/enis-dev-portfolio/',
+  base: '/enis-dev-portfolio/', // Correct base for GitHub Pages deployment
   plugins: [react()],
 })
+
